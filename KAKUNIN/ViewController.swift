ef418ff4851c844b0aef136a4122101f59179bea
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-04-22 16:28:57 +0900

    途中までつくった

--- KAKUNIN/ViewController.swift
+++ KAKUNIN/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    var number : Int = 0
+    var number2 : Int = 0
+    let device: [String] = ["iPhone", "iPad", "Apple Watch"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func backButton(){
+        if number <= -1 {
+            label.text = device[0]
+            number = -1
+
+        }else{
+            number = 0
+            label.text = device[1]
+        }
+    }
+    
+    @IBAction func next(){
+        if number >= 0 {
+            number = 1
+            label.text = device[2]
+        }else{
+            number = 0
+            label.text = device[1]
+        }
+    }
+    
+    @IBAction func firstButton(){
+        number = -1
+        label.text = device[0]
+    }
+    
+    @IBAction func secondButton(){
+        number = 0
+        label.text = device[1]
+    }
+    
+    @IBAction func thirdButton(){
+        number = 1
+        label.text = device[2]
+    }
+    
+    
 
 
 }
